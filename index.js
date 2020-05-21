5fe8a1b0fda602504e515a58f6241483b02db655
dev-jackson <angeljackson0933@gmail.com>
2020-05-21 12:33:09 -0500

    Add files

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+
+app.get('/',(req,res)=>{
+    res.end("Server running Yay!!");
+});
+
+
+//Const Socket.oi
+const io = require('socket.io')(http);
+
+io.on("connection",(userSocket)=>{
+    userSocket.on("send_stream",(data)=>{
+        userSocket.broadcast.emit("receive_stream",data);
+    });
+});
+
+http.listen(process.env.PORT);
